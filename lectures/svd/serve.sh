9bda3ca6612a5ffe7f51cd10d950d550da2fee29
SimonEnsemble <cory.simon@oregonstate.edu>
2019-12-07 11:51:51 -0800

    SVD

---
+++ lectures/svd/serve.sh
@@ -0,0 +1,2 @@
+# themes: https://github.com/hakimel/reveal.js/tree/master/css/theme
+jupyter nbconvert SVD\ tutorial.ipynb --to slides --no-prompt --TagRemovePreprocessor.remove_input_tags={\"remove_input\"} --post serve --SlidesExporter.reveal_theme=simple --SlidesExporter.reveal_scroll=True 
